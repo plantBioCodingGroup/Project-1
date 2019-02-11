a072b5cc64ee6f54dec0d17b415a1f18682c1d04
plantBioCodingGroup <plantBioCodingGroup@gmail.com>
2019-02-11 08:26:24 -0500

    Added command line parser

--- SequenceAnalyzer.py
+++ SequenceAnalyzer.py
@@ -1,15 +1,15 @@
 # reads in a fasta file and skips header lines
 import datetime
+from Bio import SeqIO as bioSeq
+import argparse
 def fileReader(file_Path):
     print("Started reading file at: {0}".format(datetime.datetime.now()))
     filePath = file_Path
     seq_list = list()
     with open(filePath, 'r') as infile:
-        for line in infile:
-            if line.startswith(">"):
-                pass
-            else:
-                seq_list.append(line)
+        for record in bioSeq.parse(infile,"fasta"):
+            seq_list.append(record.seq)
+
         print("Finished reading file at: {0}".format(datetime.datetime.now()))
     return (seq_list)
 
@@ -38,7 +38,11 @@ def getStats(seq_map):
 
 
 def main():
-    getStats(seqCounter("/Users/Jeremy/Documents/MSU/VanBuren_Lab/Grass_Drought/Eragrostis_tef/Tef_V3_5-8-18.fasta"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f","--fasta",help="Path to input fasta file",default="",required=True)
+    args = parser.parse_args()
+    fasta = args.fasta
+    getStats(seqCounter(fasta))
 
 
 if __name__ == "__main__":
@@ -46,3 +50,5 @@ if __name__ == "__main__":
     start = datetime.datetime.now()
     main()
     print("Process finished in: {0}".format((datetime.datetime.now()) - start))
+# /Users/Jeremy/Documents/MSU/VanBuren_Lab/Grass_Drought/Eragrostis_tef/Tef_V3_5-8-18.fasta
+# /Users/Jeremy/Documents/MSU/PLB_coding_group/Project-1/sequence.fasta
